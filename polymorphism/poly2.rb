7999e886c46555f52bf1bc8690b1a298e0186735
22brunda <bsrinivas@qwinix.io>
2018-04-09 17:34:57 +0530

    task

--- polymorphism/poly2.rb
+++ polymorphism/poly2.rb
@@ -20,3 +20,75 @@
   # 4
   # I am car, I have 4
   #  wheels
+# class Vehilce
+
+#    def initialize(x)
+#     puts "enter the number of wheels"
+#    end
+#   def part(wheel)
+#      @wheel = wheel
+#   end
+
+# end
+# class Bike < Vehilce
+
+#    def body_wheel(wheel)
+#     @wheel = wheel
+#   #puts  I am bike, I have 2 wheels
+#    end
+
+#   end
+#   class Car < Vehilce
+
+#      def body_wheel(wheel)
+#     @wheel = wheel
+#    # puts I am car, I have 4 wheels
+#    end
+
+#   end
+
+#   Bike = Bike.new("2")
+#   puts Bike.part
+# Car = Car.new("4")
+# puts Car.part
+ 
+
+class Vehilce 
+
+  def meets
+    puts "enter the number"
+  end
+
+  def greet(wheel)
+    log.greet
+  end
+
+  # def treat(person)
+  #   person.treat
+  # end
+
+end
+
+class Bike
+
+  def greet(wheel)
+  @wheel = wheel
+  end
+end
+
+# class Car
+
+#   def greet
+#     puts "Hi, How are you?"
+#   end
+
+#   def treat
+#     puts "Would you like to have American food?"
+#   end
+
+# end
+
+Vehilce = Vehilce.new
+log = Bike.new("2")
+Vehilce.greet(wheel)
+
