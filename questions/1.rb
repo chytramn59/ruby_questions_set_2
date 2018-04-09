f9c844a9f9d282621171a61c99cc59f723ee9d2e
22brunda <bsrinivas@qwinix.io>
2018-04-09 17:48:02 +0530

    one

--- questions/1.rb
+++ questions/1.rb
@@ -4,3 +4,9 @@
 #  {:name => "Anil" , :age => 26 , :place => "xsz"}]
 # give output as 
 # [["Brad", 27, "xyz"], ["John", 26, "xtz"], ["Anil", 26, "xsz"]
+
+k = [{:name => "Brad" , :age => 27 , :place => "xyz"} ,
+  {:name => "John" , :age => 26 , :place => "xtz"} ,
+  {:name => "Anil" , :age => 26 , :place => "xsz"}]
+
+  p k.map{|n| n.values}
--- questions/4.rb
+++ questions/4.rb
@@ -1,1 +1,8 @@
 #Print 15 times the string "Qwinix".
+class Sam
+def adding
+	15.times{puts "Qwinix"}
+	end
+end
+ k = Sam.new
+ k.adding
